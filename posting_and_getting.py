3052f14e2af960cc1ceffe3ee20117b7a673162a
HARISREE SURESH <getharisree@outlook.com>
2023-04-10 21:37:46 +0530

    Update posting_and_getting.py

--- posting_and_getting.py
+++ posting_and_getting.py
@@ -1,1 +1,26 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+stores = [{"name": "my store",
+           "items": [
+               {
+                   "name": "chair",
+                   "price": "223"
+
+               }
+           ]
+           }
+          ]
+
+
+@app.get("/store")
+def get_store():
+    return {"stores": stores}
+@app.post("/store")
+def update_store():
+    req_data = request.get_json()
+    new = {"name":req_data,"items":[]}
+    stores.append(new)
+    return new,201
 
