8eb669df2bc5adc78a629811cd0a1e80241651c8
HARISREE SURESH <getharisree@outlook.com>
2023-04-10 20:40:56 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,1 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [{"name": "my store",
+           "items": [
+               {
+                   "name": "chair",
+                   "price": "223"
+
+               }
+           ]
+           }
+          ]
+
+
+@app.get("/store")
+def get_store():
+    return {"stores": stores}
 
